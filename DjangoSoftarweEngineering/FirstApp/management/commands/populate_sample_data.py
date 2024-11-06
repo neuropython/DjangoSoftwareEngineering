edef17a9d8dfc2b93b6997aa7cb90b982310ff69
Damian <damianpietron007@gmail.com>
2024-11-06 10:38:51 +0100

    Add command for adding simple objects to DB

---
+++ DjangoSoftarweEngineering/FirstApp/management/commands/populate_sample_data.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from FirstApp.models import Product, Customer, Order
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        Customer.objects.all().delete()
+        Order.objects.all().delete()
+
+        product1 = Product.objects.create(
+        name='Book',
+        price=19.99,
+        available=True
+        )
+        customer1 = Customer.objects.create(
+        name="Damian",
+        address="NYC"
+        )
+        order1 = Order.objects.create(
+        customer=customer1,
+        status="NEW",
+        date="2021-10-10"   
+        
+        )
+
+        order1.products.add(product1)
+        self.stdout.write("Data created successfully.")
