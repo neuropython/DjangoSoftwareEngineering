7ea6a8ac30f276c4a9569e1e3319ed58014220b9
Damian <damianpietron007@gmail.com>
2024-12-04 20:16:03 +0100

    Implement serializers

--- DjangoSoftarweEngineering/DjangoSoftarweEngineering/settings.py
+++ DjangoSoftarweEngineering/DjangoSoftarweEngineering/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FirstApp'
+    'FirstApp',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
--- DjangoSoftarweEngineering/FirstApp/urls.py
+++ DjangoSoftarweEngineering/FirstApp/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
-from .views import product_list, product_detail
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CustomerViewSet, OrderViewSet
+
+
+router = DefaultRouter()
+router.register('products', ProductViewSet, basename='product')
+router.register('customers', CustomerViewSet, basename='customer')
+router.register('orders', OrderViewSet, basename='order')
 
 
 urlpatterns = [
-path('api/products/', product_list,
-name='product_list'),
-path('api/products/<int:product_id>/', product_detail,
-name='product_detail'),
+    path('api/', include(router.urls)),
 ]
