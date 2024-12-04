c7b517b47ece464999904e641c33156ab90794c5
Damian <damianpietron007@gmail.com>
2024-12-04 21:27:56 +0100

    Add MVT Django Pattern

--- DjangoSoftarweEngineering/FirstApp/urls.py
+++ DjangoSoftarweEngineering/FirstApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CustomerViewSet, OrderViewSet
-
+from .views import (ProductViewSet, 
+                    CustomerViewSet,
+                    OrderViewSet, 
+                    ProductListView, 
+                    ProductDetailView, 
+                    ProductCreateView)
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
@@ -11,4 +15,8 @@ router.register('orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('user/products/', ProductListView.as_view(), name='product_list'),
+    path('user/products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('user/products/new/', ProductCreateView.as_view(), name='product_create'),
+
 ]
